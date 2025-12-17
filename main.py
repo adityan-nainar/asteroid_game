fe416a0415a47b4c704c4526874b5a1524adda1b
adityan-nainar <adityannainar2003@gmail.com>
2025-12-17 13:22:55 +0000

    complete game

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
+from asteroids import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+import sys
+
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH} Screen height: {SCREEN_HEIGHT}")
@@ -14,8 +19,15 @@ def main():
 
     updatable  = pygame.sprite.Group()
     drawable   = pygame.sprite.Group()
-    Player.containers = (updatable, drawable)
+    asteroids   = pygame.sprite.Group()
+    shots   = pygame.sprite.Group()
 
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (shots,drawable, updatable)
+
+    asteroidfield = AsteroidField()
     player = Player(x = SCREEN_WIDTH / 2, y = SCREEN_HEIGHT / 2)
 
 
@@ -28,6 +40,17 @@ def main():
 
         for item in drawable:
             item.draw(screen)
+        
+        for obj in asteroids:
+            if obj.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
+            for shot in shots:
+                if shot.collides_with(obj):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    obj.split()
 
         updatable.update(dt)
         pygame.display.flip()
